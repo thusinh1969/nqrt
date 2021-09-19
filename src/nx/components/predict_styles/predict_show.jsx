47d48397d9221be28349b3d7e64902f78fa22fc9
thusinh1969 <thusinh1969@yahoo.com>
2021-09-19 11:26:39 +0700

    Added unmatch

--- src/nx/components/predict_styles/predict_show.jsx
+++ src/nx/components/predict_styles/predict_show.jsx
@@ -1,7 +1,7 @@
-import React, { Component } from "react"
+import React, { setState, Component } from "react"
 import './predict_show.css'
 import 'semantic-ui-css/semantic.min.css'
-import { Tab } from "semantic-ui-react"
+import { Label, Menu, Tab } from "semantic-ui-react"
 
 import PredictDescribe from './describe/describe'
 import PredictHeatmap  from './heatmap/heatmap'
@@ -11,14 +11,33 @@ class PredictShow extends Component {
 
     constructor (props) {
         super();
+
+        this.state = {
+            hasUnmatch : ''
+        }
+
     }
 
-    
+    static getDerivedStateFromProps(nextProps, prevState) {
+        console.log('PROPS at the PREDICT SHOW ?', nextProps.res)
+        if (nextProps.res) {
+            if (nextProps.res.unmatch) {
+                console.log('Does it contain UNMATCH ?', nextProps.res.unmatch)
+                return {hasUnmatch: <Label style={{'color':'yellow', 'background':'red'}}>( X )</Label>}
+            }
+            else {
+                return {hasUnmatch: ''}
+            }
+
+        }
+       }
+
 render () {
         const panes = [
             { menuItem: 'Diễn giải', render: () => <Tab.Pane><div><PredictDescribe res={this.props.res}/></div></Tab.Pane> },
             { menuItem: 'Đặc thù', render: () => <Tab.Pane><div><PredictHeatmap  res={this.props.res}/></div></Tab.Pane> },
-            { menuItem: 'Không tương đồng', render: () => <Tab.Pane><PredictUnmatch  res={this.props.res}/></Tab.Pane> },
+            { menuItem: (<Menu.Item key='unmatch'>Không tương đồng{this.state.hasUnmatch})</Menu.Item>), 
+                        render: () => <Tab.Pane><PredictUnmatch  res={this.props.res}/></Tab.Pane>},
         ]
         if (this.props.res === null){
             return null
--- src/nx/components/predict_styles/unmatch/unmatch.jsx
+++ src/nx/components/predict_styles/unmatch/unmatch.jsx
@@ -1,12 +1,40 @@
 import React, { Component } from "react"
+//import { Slide } from 'react-slideshow-image';
+//import 'react-slideshow-image/dist/styles.css'
 
+import Slider from "react-slick";
+
+import './unmatch.scss'
+
+const settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    centerMode: true,
+  };
 export default class PredictUnmatch extends Component {
     constructor(props) {
         super();
     }
-
+      
     render() {
-        return <div>
-        </div>
+        if (this.props.res.unmatch) {
+            return (
+                <div>
+                <Slider {...settings}>
+                    {console.log('UNMATCH info', this.props.res.unmatch)}
+                    <div className='mydiv'>
+                        <img className='myimg' key={0} src={`${this.props.res.unmatch.heatmap}`}/>
+                            <span>Lowest match score: <b>{`${(this.props.res.unmatch.unmatch_score*100.0).toFixed(2)}`}%</b>  {' '}
+                            ({`${this.props.res.unmatch.evidences[0].style}`} {' '}
+                             {`${(this.props.res.unmatch.evidences[0].score*100.00).toFixed(2)}`}%)</span>
+                    </div>
+                </Slider>
+                </div>
+            )
+        } else {return null}
     }
 }
+//https://react-slick.neostack.com/docs/get-started
